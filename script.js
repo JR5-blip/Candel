6b7a58a2ae74d24729da42cff004eed4a1a4ccb1
JR5-blip <abubakerabdelhavez@gmail.com>
2025-07-31 16:13:17 +0300

    script.js

--- script.js
+++ script.js
@@ -3,14 +3,17 @@ let candle = document.getElementById('candle');
 let message = document.getElementById('message');
 window.onload = function () {
 const candle = document.getElementById('candle');
+const prompt = document.getElementById('prompt');
 let clickCount = 0;
 
 setTimeout(() => {
   candle.src = 'candle_off.png';
+  prompt.style.display = 'block';
 }, 2000); // ⏳ Turns off after 2 seconds
 candle.addEventListener('click', () => {
   clickCount++;
   if (clickCount === 1) {
+    prompt.style.display = 'none';
     candle.src = 'candle_on.png';
     setTimeout(() => {
       candle.src = 'candle_off.png';
